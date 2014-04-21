8fc53042cf296ed3ac2797cfce32bf704ccd8c19
Arvin Domingo <arvin.o.domingo@gmail.com>
2014-04-21 22:02:46 +0800

    fixes

--- x-community-2-visits.php
+++ x-community-2-visits.php
@@ -11,6 +11,7 @@ if($file_headers[0] == 'HTTP/1.1 404 Not Found') {
 
 $visit = x::visit();
 $visit_name = array('오늘', '어제', '최대', '전체');
+
 ?>
 
 <div class='stats-container'>
@@ -18,8 +19,11 @@ $visit_name = array('오늘', '어제', '최대', '전체');
 		<img src="<?=$icon_url?>"/>접속자 통계
 	</div>
 	<div class='stats-content'>
-		<?for( $i=0; $i<=3; $i++) { ?>
-			<div class='per-stats'><?=$visit_name[$i]?> <span class='stats-results'><?=number_format($visit[$i+1])?></span></div>
+		<?for( $i=0; $i<=3; $i++) { 
+			if( $visit[$i+1] ) $num_of_visits = number_format($visit[$i+1]);
+			else $num_of_visits = 0;
+		?>
+			<div class='per-stats'><?=$visit_name[$i]?> <span class='stats-results'><?=$num_of_visits?></span></div>
 		<?}?>
 	</div>
 	<?php if ($is_admin == "super") {  ?><a class='view-more-stats' href="<?php echo G5_ADMIN_URL ?>/visit_list.php">자세히</a><?php } ?>
